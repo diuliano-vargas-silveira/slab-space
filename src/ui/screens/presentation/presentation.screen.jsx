1be525c7f3e3e4b8730074df0ef03d3909986c78
Diuliano Vargas da Silveira <diulidsvargas@gmail.com>
2023-04-27 11:10:56 -0300

    resposivity in website

--- src/ui/screens/presentation/presentation.screen.jsx
+++ src/ui/screens/presentation/presentation.screen.jsx
@@ -33,25 +33,27 @@ const Presentation = () => {
 
   return (
     <section className="t-start-page__presentation">
-      <h2 className="t-start-page__presentation__title">
-        EXPLORE THE UNIVERSE AND FEEL THE SENSATION
-      </h2>
-      <p className="t-start-page__presentation__text">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
-        dignissimos odit enim sed eos, magni eum eius ex totam obcaecati
-        eligendi, quibusdam sint amet!
-      </p>
-      <div className="t-start-page__presentation__buttons">
-        <Button className="t-start-page__presentation__button" suffix="P">
-          Watch Space Agency
-        </Button>
-        <Button
-          className="t-start-page__presentation__button-ticket"
-          suffix="A"
-        >
-          Get Ticket
-        </Button>
-      </div>
+      <section className="t-start-page__text">
+        <h2 className="t-start-page__presentation__title">
+          EXPLORE THE UNIVERSE AND FEEL THE SENSATION
+        </h2>
+        <p className="t-start-page__presentation__text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
+          dignissimos odit enim sed eos, magni eum eius ex totam obcaecati
+          eligendi, quibusdam sint amet!
+        </p>
+        <div className="t-start-page__presentation__buttons">
+          <Button className="t-start-page__presentation__button" suffix="P">
+            Watch Space Agency
+          </Button>
+          <Button
+            className="t-start-page__presentation__button-ticket"
+            suffix="A"
+          >
+            Get Ticket
+          </Button>
+        </div>
+      </section>
       <SocialInformations />
 
       <img
